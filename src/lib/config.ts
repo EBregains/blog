9b68b37b54812894e45efa2d88c0046ff88f57f6
EBregains <ebregains@gmail.com>
2023-05-09 12:53:15 -0400

    change title

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import { dev } from "$app/environment";
 
-export const title = 'blog';
+export const title = 'ebregainsBlog';
 export const description = 'blog';
 export const url = dev ? 'http://localhost:5173' : 'https://blog.com';
